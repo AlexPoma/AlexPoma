390df2089c2aa386b37c6d35dcf06664fa50c4ad
Alex <alex.poma.iis@gmail.com>
2021-03-16 00:11:24 -0500

    Actulización Isolate

---
+++ assets/js/isotope.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+	
+    var $container = $('.isotope');
+    // console.log($('.isotope'));
+    $container.imagesLoaded(function () {
+        $('.isotope').isotope({
+            itemSelector: '.isotope-item',
+            layoutMode: 'fitRows',
+        });
+    });
+    
+    // filter items on click
+    $('#filters').on( 'click', '.type', function() {
+        var filterValue = $(this).attr('data-filter');
+        console.log($('.filterValue'));
+        $container.isotope({ filter: filterValue });
+    });
+    
+    // change is-checked class on buttons
+    $('.filters').each( function( i, typeGroup ) {
+        var $typeGroup = $( typeGroup );
+        $typeGroup.on( 'click', '.type', function() {
+          $typeGroup.find('.active').removeClass('active');
+          $( this ).addClass('active');
+        });
+    });
+});
