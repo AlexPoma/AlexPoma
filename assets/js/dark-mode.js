7da8f75b9537492f00524079fcc46453598ca9e4
Alex <alex.poma.iis@gmail.com>
2021-05-04 23:51:53 -0500

    cv2

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -4,14 +4,18 @@ $(document).ready(function() {
     if(!localStorage.getItem("DarkModeVar")){
         localStorage.setItem("DarkModeVar","Ligth");
     }
+    
+    var a_link = document.getElementById('resume-button_href');
 
     let def = localStorage.getItem("DarkModeVar");
     
     if (def == "Dark"){
         $('body').addClass('dark-mode');
+        a_link.setAttribute("href", "assets/pdf/resume-dark.pdf");
     }
     else{
         $('body').removeClass('dark-mode');
+        a_link.setAttribute("href", "assets/pdf/resume-light.pdf");
     }
 
     document.getElementById("darkmode").checked = (localStorage.getItem("DarkModeVar")=="Dark")?true:false;
@@ -22,10 +26,12 @@ $(document).ready(function() {
 		if ($(this).is(':checked')) {
 			$('body').addClass('dark-mode');
 			localStorage.setItem("DarkModeVar","Dark");
+            a_link.setAttribute("href", "assets/pdf/resume-dark.pdf");
 		} 
         else {
 			$('body').removeClass('dark-mode');
 			localStorage.setItem("DarkModeVar","Ligth");
+            a_link.setAttribute("href", "assets/pdf/resume-light.pdf");
 		}
         // console.log(localStorage.getItem("DarkModeVar"));
 	});
