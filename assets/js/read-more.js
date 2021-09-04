a07b8d77d97647cbc0504f2827c031ea25cb1c01
Alex <alex.poma.iis@gmail.com>
2021-09-03 23:55:38 -0500

    Updt Jobs

--- assets/js/read-more.js
+++ assets/js/read-more.js
@@ -44,4 +44,27 @@ $(document).ready(function () {
             }
         }
     });
+
+    $(".JOB_03 #toggle_read_more").click(function () {
+        var elem = $(".JOB_03 #toggle_read_more").text();
+        let def = localStorage.getItem("lang_strg");
+        if (def == 'en') {
+            if (elem == "Read More") {
+                $(".JOB_03 #toggle_read_more").text("Read Less");
+                $(".JOB_03 #content_read_more").slideDown();
+            } else {
+                $(".JOB_03 #toggle_read_more").text("Read More");
+                $(".JOB_03 #content_read_more").slideUp();
+            }
+        }
+        if (def == 'es') {
+            if (elem == "Leer Más") {
+                $(".JOB_03 #toggle_read_more").text("Leer Menos");
+                $(".JOB_03 #content_read_more").slideDown();
+            } else {
+                $(".JOB_03 #toggle_read_more").text("Leer Más");
+                $(".JOB_03 #content_read_more").slideUp();
+            }
+        }
+    });
 });
